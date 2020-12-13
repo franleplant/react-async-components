8f01714abdff427441323038a773343d703bd8b2
Fran Guijarro <franleplant@gmail.com>
2020-12-13 11:51:52 -0300

    autoformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  const [userId, setUserId] = useState("fran")
+  const [userId, setUserId] = useState("fran");
   return (
     <div>
-
       <div className="card">
-        <button onClick={() => setUserId(userId => userId + "x")}>fetch other user</button>
+        <button onClick={() => setUserId((userId) => userId + "x")}>
+          fetch other user
+        </button>
         <div>
-          <AsyncContainer childProps={{userId}} fallback={() => <Loading/>}>
+          <AsyncContainer childProps={{ userId }} fallback={() => <Loading />}>
             {AsyncComponent}
           </AsyncContainer>
         </div>
       </div>
 
       <div className="card">
-        <AsyncContainer childProps fallback={() => <Loading/>} renderError={(err) => <span>ERROR {err}</span>}>
+        <AsyncContainer
+          childProps
+          fallback={() => <Loading />}
+          renderError={(err) => <span>ERROR {err}</span>}
+        >
           {AsyncComponentWithError}
         </AsyncContainer>
       </div>
@@ -24,80 +29,84 @@ export default function App() {
 }
 
 interface IContainerProps<TChildProps> {
-  children: (props: TChildProps) => Promise<React.ReactElement>
-  childProps: TChildProps
-  fallback?: () => React.ReactElement
-  renderError?: (err: any) => React.ReactElement
+  children: (props: TChildProps) => Promise<React.ReactElement>;
+  childProps: TChildProps;
+  fallback?: () => React.ReactElement;
+  renderError?: (err: any) => React.ReactElement;
 }
 
-export function AsyncContainer<TChildProps = void>(props: IContainerProps<TChildProps>): React.ReactElement | null {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<any>(null)
-  const [element, setElement] = useState<React.ReactElement | null>(null)
+export function AsyncContainer<TChildProps = void>(
+  props: IContainerProps<TChildProps>
+): React.ReactElement | null {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<any>(null);
+  const [element, setElement] = useState<React.ReactElement | null>(null);
 
-  const {childProps, children} = props
+  const { childProps, children } = props;
 
   useEffect(() => {
     async function run() {
-      setLoading(true)
-      setError(null)
+      setLoading(true);
+      setError(null);
       try {
-        const element = await children(childProps)
-        setElement(element)
+        const element = await children(childProps);
+        setElement(element);
       } catch (err) {
-        setError(err)
+        setError(err);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     }
 
-    run()
-  }, [childProps, children])
+    run();
+  }, [childProps, children]);
 
   if (loading && props.fallback) {
-    return props.fallback()
+    return props.fallback();
   }
 
   if (error && props.renderError) {
-    return props.renderError(error)
+    return props.renderError(error);
   }
 
-  return element
+  return element;
 }
 
 interface IProps {
-  userId: string
+  userId: string;
 }
 
-
-export async function AsyncComponent(props: IProps): Promise<React.ReactElement> {
-  const user = await getUser(props.userId)
-  return <div>
-    fetched user
-    <pre>
-      {JSON.stringify(user, null, 2)}
-    </pre>
-  </div>
+export async function AsyncComponent(
+  props: IProps
+): Promise<React.ReactElement> {
+  const user = await getUser(props.userId);
+  return (
+    <div>
+      fetched user
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
 }
 
 export async function AsyncComponentWithError(): Promise<React.ReactElement> {
-  await new Promise((resolve, reject) => setTimeout(() => reject("failed"), 1000))
-  return <div>should never render</div>
+  await new Promise((resolve, reject) =>
+    setTimeout(() => reject("failed"), 1000)
+  );
+  return <div>should never render</div>;
 }
 
 export function Loading() {
-  return <span>Loading...</span>
+  return <span>Loading...</span>;
 }
 
 const user = {
   birthday: "1988-11-01",
   github: "franleplant",
   twitter: "franleplant",
-}
+};
 
-type IUser = typeof user
+type IUser = typeof user;
 
 export function getUser(userId: string): Promise<IUser> {
- return new Promise(resolve => setTimeout(() => resolve(user), 1000))
+  return new Promise((resolve) => setTimeout(() => resolve(user), 1000));
 }
-
